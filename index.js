7cf7d34c6780978b8e8c5da53d6b8d4f9ec4d2b4
lexrock <alexhiglesias@gmail.com>
2025-07-31 12:14:07 +0200

    Docker funcionando con IOTA, configuración inicial estable

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+// index.js
 console.log("IOTA dev environment running");
+
+// Aquí es donde más adelante añadirás la lógica de conexión con el nodo Hornet
+// Ejemplo (futuro):
+// const { Client } = require('@iota/sdk');
+// const client = new Client({ nodes: ['http://hornet:14265'] });
+
+/**
+ * Mantener el contenedor en ejecución.
+ * Este bucle vacío evita que el contenedor se apague después de ejecutar el script.
+ */
+setInterval(() => {
+  // Aquí podrías añadir lógica de comprobación cada cierto tiempo si quisieras
+}, 1000);
